5f3da840ff38a77b8c38341f64051ecfddfe163e
Lucas Guidi <lucasrguidi@gmail.com>
2025-03-21 19:08:08 -0300

    chore: :wrench: config docker and typeorm

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+if (!process.env.DATABASE_PORT) {
+  throw new Error('DATABASE_HOST is not set in env');
+}
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true, // Development mode
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
