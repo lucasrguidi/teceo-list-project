22c9a3414332fe17d5a5143b34a1af047f51354e
Lucas Guidi <lucasrguidi@gmail.com>
2025-03-22 10:44:50 -0300

    chore: :wrench: config eslint and prettier

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
 import HomePage from "./pages/home-page";
-
-
 
 export default function App() {
   return (
     <HomePage />
-  );
+  )
 }
--- react-frontend/src/pages/home-page.tsx
+++ react-frontend/src/pages/home-page.tsx
@@ -1,18 +1,22 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-
 
 export default function HomePage() {
   return (
     <AppBar position="static">
-  <Toolbar variant="dense">
-    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" color="inherit" component="div">
-      Photos
-    </Typography>
-  </Toolbar>
-</AppBar>
-  )
+      <Toolbar variant="dense">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          Photos
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
