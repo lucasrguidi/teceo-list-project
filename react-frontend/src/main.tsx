ba696b2693622151d3f897ec066b1112cb3b08c1
Lucas Guidi <lucasrguidi@gmail.com>
2025-03-22 10:56:40 -0300

    chore: :wrench: fix eslint

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -6,9 +6,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-      <CssBaseline />
-      <App />
-
+    <CssBaseline />
+    <App />
   </React.StrictMode>,
 );
