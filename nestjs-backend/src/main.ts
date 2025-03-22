1582af42ffa746f3e0d7b90e7bcaa79e674185f1
Lucas Guidi <lucasrguidi@gmail.com>
2025-03-22 12:10:11 -0300

    feat: :sparkles: (backend) config cors

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
